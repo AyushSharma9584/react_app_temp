d9a531880702eb5843bd3ff994de6b1ee839b36c
Ayush sharma <ayushsharma83499@gmail.com>
2024-10-26 00:53:33 +0530

    changes

--- temp_pro/src/App.js
+++ temp_pro/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [first, setfirst] = useState("")
+  useEffect(() => {
+    hitApi()
+  })
+  const hitApi = async () => {
+    const result = await fetch('https://api.sarteinfomining.com/auth/api/test')
+    const data = await result.json()
+    console.log(data)
+    setfirst(data.message)
+  }
   return (
     <div className="App">
       <p>Wellcome everyone !</p>
+      <p>{first}</p>
 
     </div>
   );
